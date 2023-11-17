a73f940a5603a6d4981f4736ea583d9380de4509
Vyacheslav <mepa087@gmail.com>
2023-11-17 09:56:08 +0300

    add composer in project

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
 	declare(strict_types=1);
 	session_start();
+	include_once './vendor/autoload.php';
 if (empty($_SESSION['user'])){
 	$user=false;
 } else {
 	$user = true;
 }
-	include './form.html';
+	include_once './form.html';
 
